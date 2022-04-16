9cfa83c61778cc84ed172ec3509612fb40aa9be1
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-04-16 20:46:12 +0530

    2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ import "./styles.css";
 const App = () => {
   const [count, setCount] = useState(0);
   return (
+    <>
+    <h1 style={{color:"white",marginBottom:"100px",marginRight:"400px"}}>React Fremar-Motion Animation</h1>
+    
     <MotionConfig reducedMotion="user">
+     
       <Refresh onClick={() => setCount(count + 1)} />
       <div className="example-container">
         <motion.div
@@ -28,7 +32,29 @@ const App = () => {
         />
       </div>
     </MotionConfig>
+
+
+    <Refresh onClick={() => setCount(count + 1)} />
+      <div className="example-containe">
+        <motion.div
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 270, 270, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+          }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+            repeat: Infinity,
+            repeatDelay: 1
+          }}
+        />
+      </div>
+    </>
   );
+
+  
 };
 
 render(<App />, document.getElementById("root"));
